174fb0e80f960bd06404ea1028f7128cb68bc5ca
adhikarisushant <sushant.adhikari.619@gmail.com>
2023-07-06 14:01:10 +0545

    residue

---
+++ server/controllers/userController.js
@@ -0,0 +1,25 @@
+import asyncHandler from 'express-async-handler';
+import db from '../db/index.js'
+
+// @desc Auth user & get token
+// @route POST /api/v1/users/auth
+// @access Public
+
+const authUser = asyncHandler(async (req, res) => {
+    const {name, password} = req.body;
+    
+});
+
+// @desc Register a new user
+// @route POST /api/users
+// @access Public 
+
+const registerUser = asyncHandler(async (req, res) => {
+    const { name, email, password } = req.body;
+});
+
+
+export {
+    authUser,
+    registerUser
+};
--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ import db from './db/index.js';
 import morgan from 'morgan';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
+import { errorHandler, notFound } from './middleware/errorMiddleware.js';
 
 const port = process.env.PORT || 5000;
 const app = express();
@@ -16,8 +17,36 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(cookieParser());
 
-app.get('/', (req, res) => {
-    res.send('API is running...');
-});
+
+if (process.env.NODE_ENV === 'production') {
+    const __dirname = path.resolve();
+    app.use(express.static(path.join(__dirname, '/client/dist')));
+  
+    app.get('*', (req, res) =>
+      res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'))
+    );
+  } else {
+    app.get('/', async (req, res) => {
+      try { 
+        const getUsers = await db.query("select * from users;")
+
+        res.status(200).json({
+            status: "success",
+            results: getUsers.rows.length,
+            data:{
+                users: getUsers.rows,
+            },
+        });
+
+      } catch(err) {
+          console.log(err);
+      }
+      
+        // res.send('API is running....');
+    });
+}
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));

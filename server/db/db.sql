4de32fcd0b531a8ef8091acae938357e6dd5b801
adhikarisushant <sushant.adhikari.619@gmail.com>
2023-12-12 20:26:20 +0545

    db table querieso

--- server/db/db.sql
+++ server/db/db.sql
@@ -1,1 +1,77 @@
-CREATE TABLE;
+CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR (50) UNIQUE NOT NULL,
+    email VARCHAR (255) UNIQUE NOT NULL,
+    role VARCHAR (50) NOT NULL DEFAULT 'isAdmin',  
+    password VARCHAR (255) NOT NULL,
+    status BOOLEAN NOT NULL DEFAULT TRUE
+);
+CREATE TABLE IF NOT EXISTS customers (
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR (50) UNIQUE NOT NULL,
+    contact VARCHAR (50) NOT NULL,
+    address VARCHAR (100) NOT NULL,
+    status BOOLEAN NOT NULL DEFAULT TRUE,
+    created_by INTEGER REFERENCES users(id),
+    updated_by INTEGER REFERENCES users(id),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP 
+);
+
+CREATE TABLE IF NOT EXISTS vendors (
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,    
+    name VARCHAR (255) NOT NULL,
+    cow_price NUMERIC (5, 2),
+    buff_price NUMERIC (5, 2),
+    contact VARCHAR (50) NOT NULL,
+    address VARCHAR (100) NOT NULL,
+    status BOOLEAN NOT NULL DEFAULT TRUE,
+    created_by INTEGER REFERENCES users(id),
+    updated_by INTEGER REFERENCES users(id),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS products (
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR (100) UNIQUE NOT NULL,
+    price NUMERIC (6, 2) NOT NULL,
+    status BOOLEAN NOT NULL DEFAULT TRUE,
+    created_by INTEGER REFERENCES users(id),
+    updated_by INTEGER REFERENCES users(id),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- CREATE TYPE product_enum AS ENUM ('buff', 'cow');
+
+CREATE TABLE IF NOT EXISTS vendor_transactions (
+        id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+        vendor_id INTEGER REFERENCES vendors(id),
+        quantity NUMERIC (5, 2) NOT NULL,
+        product_type product_enum,
+        price NUMERIC (5, 2) NOT NULL,
+        amount NUMERIC (6, 2) NOT NULL,
+        payment_status BOOLEAN NOT NULL DEFAULT FALSE,
+        lactometer SMALLINT,
+        status BOOLEAN NOT NULL DEFAULT TRUE,
+        created_by INTEGER REFERENCES users(id),
+        updated_by INTEGER REFERENCES users(id),
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS customer_transactions (
+        id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+        customer_id INTEGER REFERENCES customers(id),
+        product_id INTEGER REFERENCES products(id),
+        quantity NUMERIC (5, 2) NOT NULL,
+        price NUMERIC (5, 2) NOT NULL,
+        amount NUMERIC (6, 2) NOT NULL,
+        payment_status BOOLEAN NOT NULL DEFAULT FALSE,
+        status BOOLEAN NOT NULL DEFAULT TRUE,
+        created_by INTEGER REFERENCES users(id),
+        updated_by INTEGER REFERENCES users(id),
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
--- server/server.js
+++ server/server.js
@@ -1,49 +1,47 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
-import express from 'express';
-import db from './db/index.js';
-import morgan from 'morgan';
-import cors from 'cors';
-import cookieParser from 'cookie-parser';
-import { errorHandler, notFound } from './middleware/errorMiddleware.js';
+import express from "express";
+import db from "./db/index.js";
+import morgan from "morgan";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
 
 const port = process.env.PORT || 5000;
 const app = express();
 
 app.use(cors());
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cookieParser());
 
+if (process.env.NODE_ENV === "production") {
+  const __dirname = path.resolve();
+  app.use(express.static(path.join(__dirname, "/client/dist")));
 
-if (process.env.NODE_ENV === 'production') {
-    const __dirname = path.resolve();
-    app.use(express.static(path.join(__dirname, '/client/dist')));
-  
-    app.get('*', (req, res) =>
-      res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'))
-    );
-  } else {
-    app.get('/', async (req, res) => {
-      try { 
-        const getUsers = await db.query("select * from users;")
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"))
+  );
+} else {
+  app.get("/", async (req, res) => {
+    try {
+      const getUsers = await db.query("select * from users;");
 
-        res.status(200).json({
-            status: "success",
-            results: getUsers.rows.length,
-            data:{
-                users: getUsers.rows,
-            },
-        });
+      res.status(200).json({
+        status: "success",
+        results: getUsers.rows.length,
+        data: {
+          users: getUsers.rows,
+        },
+      });
+    } catch (err) {
+      console.log(err);
+    }
 
-      } catch(err) {
-          console.log(err);
-      }
-      
-        // res.send('API is running....');
-    });
+    // res.send('API is running....');
+  });
 }
 
 app.use(notFound);
